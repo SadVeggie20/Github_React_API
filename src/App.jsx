8525a3e8f4058d7aa3dc148070eb97cccd265ad1
[SadVeggie20] <[orlando_lechuga125@hotmail.com]>
2023-07-01 13:24:45 -0600

    css_changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,12 @@ function App() {
 
   return (
     <>
+    <div className='mainText-container'>
       <h1>Github React Issues</h1>
+      <p>
+        Esta es una pagina que utiliza la API de github para ver los Hilos relacionados con los problemas existentes en React
+      </p>
+    </div>
     </>
   )
 }
